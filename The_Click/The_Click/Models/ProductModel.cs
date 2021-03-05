42b92284eb8c40650c9f6310bb849c53d2087242
Rueli <Alsklingen2308>
2021-03-05 14:12:50 -0500

    COMP2139-The_Click

---
+++ The_Click/The_Click/Models/ProductModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace The_Click.Models
+{
+    public class ProductModel
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+       // [DataType(DataType.Date)]//
+       public DateTime ReleaseDate { get; set; }
+
+       
+    }
+
+}
